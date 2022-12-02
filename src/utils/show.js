f8fbac2810b0d27ed91f12294f96418f5e13a99d
djy <dingjiaying@shenniudata.com>
2022-12-02 22:53:50 +0800

    首页bannner切换

--- src/utils/show.js
+++ src/utils/show.js
@@ -1,10 +1,13 @@
-
-export default function(top1,top2,top3) {
-    setTimeout(() => {
+function show(top1, top2, top3) {
+    var a = null
+     a = setTimeout(() => {
         top1.style.opacity = 0;
         top2.style.opacity = 1;
         top3.style.opacity = 0;
-        console.log("第二张图显示");
+        console.log("第二张图显示")
+   
+        clearTimeout(a)
+      
         setTimeout(() => {
             top1.style.opacity = 0;
             top2.style.opacity = 0;
@@ -14,8 +17,14 @@ export default function(top1,top2,top3) {
                 top1.style.opacity = 1;
                 top2.style.opacity = 0;
                 top3.style.opacity = 0;
-                console.log("第一张图显示");
-            }, 3000)
-        }, 3000)
-    }, 3000);
+                console.log("第一张图显示")
+                show(top1, top2, top3)
+            }, 4000)
+        }, 4000)
+    }, 4000);
+
+    console.log(a);
 }
+
+
+export default show
--- src/views/banner.vue
+++ src/views/banner.vue
@@ -1,127 +1,68 @@
 <template>
-    <div class="banner">
-        <div class="top">
-            <div class="top1"></div>
-            <div class="top2"></div>
-            <div class="top3"></div>
-        </div>
-
-        <div class="bottom">2</div>
+  <div class="banner">
+    <div class="top">
+      <div class="top1"></div>
+      <div class="top2"></div>
+      <div class="top3"></div>
     </div>
+
+    <div class="bottom">2</div>
+  </div>
 </template>
 
 <script>
-// import show from '../utils/show'
+import show from '../utils/show'
 export default {
-    name: 'Banner',
-    data() {
-        return {
-
-        }
-    },
-    mounted() {
-        let top1 = document.querySelector('.top1')
-        let top2 = document.querySelector('.top2')
-        let top3 = document.querySelector('.top3')
-        // setInterval(() => {
-        //     top1.style.opacity = 0;
-        //     top2.style.opacity = 1;
-        //     top3.style.opacity = 0;
-        //     console.log("第二张图显示");
-        //     setInterval(() => {
-        //         top1.style.opacity = 0;
-        //         top2.style.opacity = 0;
-        //         top3.style.opacity = 1;
-        //         console.log("第三张图显示");
-        //            setInterval(() => {
-        //             top1.style.opacity = 1;
-        //             top2.style.opacity = 0;
-        //             top3.style.opacity = 0;
-        //             console.log("第一张图显示");
-        //            
-        //         })
-        //     }, 2000)
-        // }, 3000);
-
-
-        setTimeout(() => {
-            top1.style.opacity = 0;
-            top2.style.opacity = 1;
-            top3.style.opacity = 0;
-            console.log("第二张图显示");
-            setTimeout(() => {
-                top1.style.opacity = 0;
-                top2.style.opacity = 0;
-                top3.style.opacity = 1;
-                console.log("第三张图显示");
-                setTimeout(() => {
-                    top1.style.opacity = 1;
-                    top2.style.opacity = 0;
-                    top3.style.opacity = 0;
-                    console.log("第一张图显示");
-                    //在这里调用它自己  
-                }, 3000)
-            }, 3000)
-        }, 3000);
-
-
-
-
-        // setInterval(() => {
-        //     top1.style.opacity = 0;
-        //     top2.style.opacity = 0;
-        //     top3.style.opacity = 1;
-        //     console.log("第三张图显示");
-        // }, 5000);
-
-        // setInterval(() => {
-        //     top1.style.opacity = 1;
-        //     top2.style.opacity = 0;
-        //     top3.style.opacity = 0;
-        //     console.log("第一张图显示");
-        // }, 7000);
-    }
-}
+  name: "Banner",
+  data() {
+    return {
+      i: 1,
+    };
+  },
+  mounted() {
+    let top1 = document.querySelector(".top1");
+    let top2 = document.querySelector(".top2");
+    let top3 = document.querySelector(".top3");
+    show(top1,top2,top3)
+   
+  },
+};
 </script>
 
 <style scoped>
 .top {
-    position: relative;
+  position: relative;
 }
 
 .top1 {
-    opacity: 1;
-    transition: all 0.6s;
-    position: absolute;
-    width: 100%;
-    height: 460px;
-    background: url('https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMS8wZDJlOGJhYjNhYTllOTEzNDM5YzA3NDM4MDk4OTcwNC05MC53ZWJw.webp');
+  opacity: 1;
+  transition: all 1s;
+  position: absolute;
+  width: 100%;
+  height: 460px;
+  background: url("https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMS8wZDJlOGJhYjNhYTllOTEzNDM5YzA3NDM4MDk4OTcwNC05MC53ZWJw.webp");
 }
 
 .top2 {
-    opacity: 0;
-    transition: all 0.6s;
-    position: absolute;
-    width: 100%;
-    height: 460px;
-    background: url('https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMC9jOTNjM2FjZjQ1MzM5YTgzZDU0YjdmMTY1NzY4YmE5NC05MC53ZWJw.webp');
+  opacity: 0;
+  transition: all 1s;
+  position: absolute;
+  width: 100%;
+  height: 460px;
+  background: url("https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMC9jOTNjM2FjZjQ1MzM5YTgzZDU0YjdmMTY1NzY4YmE5NC05MC53ZWJw.webp");
 }
 
 .top3 {
-    opacity: 0;
-    transition: all 0.6s;
-    position: absolute;
-    width: 100%;
-    height: 460px;
-    background: url('https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMS84NGI3MTgzMTQ0NWVkZWEyODMzOTRkMGI4NjI3ZTJmMS05MC53ZWJw.webp');
-}
-
-#topp {
-    transition: all 0.6s;
+  opacity: 0;
+  transition: all 1s;
+  position: absolute;
+  width: 100%;
+  height: 460px;
+  background: url("https://ccdn.goodq.top/caches/4ce61cd756c0c3467de0977d6849043b/aHR0cDovLzU3ZWEyMzYwMzY5YjUudDczLnFpZmVpeWUuY29tL3FmeS1jb250ZW50L3VwbG9hZHMvMjAxNi8xMS84NGI3MTgzMTQ0NWVkZWEyODMzOTRkMGI4NjI3ZTJmMS05MC53ZWJw.webp");
 }
 
 .banner {
-    height: 600px;
-    margin-bottom: 400px;
+  height: 600px;
+  margin-bottom: 400px;
 }
 </style>
